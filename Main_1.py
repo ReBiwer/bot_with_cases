7d41b0d343060c137f55c7315b3cfbadfecc8729
Комп с работы <vovka1998@gmail.com>
2024-01-24 16:49:37 +0300

    update files

--- Main_1.py
+++ Main_1.py
@@ -5,10 +5,12 @@ import json
 bot = TeleBot('6845585691:AAHvWslzRvwxq9LEzhdX7vKo1vEpkwPN35w')
 API = '44fb8db4a710c0322de424c1a66779b7'
 
-@bot.message_handler(commands = ['start'])
+
+@bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'Приветствую, рад вас видеть! '
                                       'Напишите название города в котором вы хотите узнать погоду!')
+
 
 @bot.message_handler(content_types=['text'])
 def get_weather(message):
@@ -19,7 +21,7 @@ def get_weather(message):
         temp_in_city = data["main"]["temp"]
         bot.reply_to(message, f'Погода в городе {city.capitalize()}: {temp_in_city}')
         image = 'sunny_weather.jpg' if temp_in_city < 5.0 else 'sun_weather.jpg'
-        with open('images/'+image, 'rb') as file:
+        with open('images/' + image, 'rb') as file:
             bot.send_photo(message.chat.id, file)
     else:
         bot.send_message(message.chat.id, 'Город указан не верно')
