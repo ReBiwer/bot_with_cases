b9b35d550e316cfb1aefa194a6b678e0a4a5c597
Комп с работы <vovka1998@gmail.com>
2024-01-26 13:24:16 +0300

    Настроил сценарий с получением погоды повторно

--- handlers/custom_handlers/get_weather.py
+++ handlers/custom_handlers/get_weather.py
@@ -16,9 +16,7 @@ def get_weather(message: Message):
 @bot.callback_query_handler(func=lambda call: call.data == 'да')
 def get_weather_again(call):
     message = call.message
-    bot.set_state(message.from_user.id, GettingWeather.city, message.chat.id)
-    bot.send_message(message.chat.id, message.text)
-    bot.send_message(message.chat.id, 'Введите город где вы хотите узнать погоду')
+    bot.send_message(message.chat.id, 'Снова введите город где вы хотите узнать погоду')
 
 
 @bot.message_handler(state=GettingWeather.city)
@@ -32,7 +30,6 @@ def put_weather(message: Message):
         image = 'sunny_weather.jpg' if temp_in_city < 5.0 else 'sun_weather.jpg'
         with open('images/' + image, 'rb') as file:
             bot.send_photo(message.chat.id, file)
-        bot.delete_state(message.from_user.id, message.chat.id)
 
         bot.send_message(message.chat.id, 'Хотите узнать погоду в другом городе?',
                          reply_markup=keyboard_inline('да', 'нет'))
@@ -43,4 +40,5 @@ def put_weather(message: Message):
 @bot.callback_query_handler(func=lambda call: call.data == 'нет')
 def restart(call):
     message = call.message
+    bot.delete_state(message.chat.id)
     bot.send_message(message.chat.id, 'Выберите нужную команду в меню')
