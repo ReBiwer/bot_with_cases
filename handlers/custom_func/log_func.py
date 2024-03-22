cf377e58bab41b55e4b5653f957aa9431d59cf7c
Комп с работы <vovka1998@gmail.com>
2024-03-22 17:00:04 +0300

    Исправил опечатку в коде. При логирование админ. действий не верно были указаны атрибуты (поле) БД с логами действий

--- handlers/custom_func/log_func.py
+++ handlers/custom_func/log_func.py
@@ -2,21 +2,20 @@ from database.logging_users import UserAction
 from database.logging_admins import AdminAction
 from datetime import datetime
 from telebot.types import Message
-from .check_admin import check_admin_status
 
 
 def log_action(action, message: Message, check_admin=False):
     if check_admin:
-        admin_action = AdminAction(id_admin=message.from_user.id,
-                                   usernam_admin=message.from_user.username,
-                                   action=action,
-                                   time_action=datetime.now(),
-                                   )
+        admin_action = AdminAction.create(id_admin=message.from_user.id,
+                                          username_admin=message.from_user.username,
+                                          action_admin=action,
+                                          time_action=datetime.now(),
+                                          )
         admin_action.save()
     else:
-        user_action = UserAction(id_user=message.from_user.id,
-                                 username=message.from_user.username,
-                                 action=action,
-                                 time_action=datetime.now(),
-                                 )
+        user_action = UserAction.create(id_user=message.from_user.id,
+                                        username=message.from_user.username,
+                                        action=action,
+                                        time_action=datetime.now(),
+                                        )
         user_action.save()
