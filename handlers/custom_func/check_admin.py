7172b2a126d5e89c39584fccc4d89f5977762709
Комп с работы <vovka1998@gmail.com>
2024-03-22 16:21:12 +0300

    Ренейм некоторых элементов

--- handlers/custom_func/check_admin.py
+++ handlers/custom_func/check_admin.py
@@ -1,9 +1,9 @@
-from database.list_admins import Admin
+from database.list_admins import Admins
 from telebot.types import Message
 
 
 def check_admin_status(message: Message):
-    id_all_admins = [admin.id_admin for admin in Admin.select()]
+    id_all_admins = [admin.id_admin for admin in Admins.select()]
     id_user = message.from_user.id
     if id_user in id_all_admins:
         return True
