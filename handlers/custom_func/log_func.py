8ab0fc5a5bdeae8755841002faa8e2fd3d4e076b
Комп с работы <vovka1998@gmail.com>
2024-03-22 16:19:53 +0300

    Доработал функцию логирования

--- handlers/custom_func/log_func.py
+++ handlers/custom_func/log_func.py
@@ -1,12 +1,22 @@
-from database.logging import User
+from database.logging_users import UserAction
+from database.logging_admins import AdminAction
 from datetime import datetime
-from states.getting_weather import GettingWeather
+from telebot.types import Message
+from .check_admin import check_admin_status
 
 
-def log_action(action):
-    user = User(id_user=GettingWeather.id_user,
-                username=GettingWeather.username_user,
-                action=action,
-                time_action=datetime.now(),
-                )
-    user.save()
+def log_action(action, message: Message, check_admin=False):
+    if check_admin:
+        admin_action = AdminAction(id_admin=message.from_user.id,
+                                   usernam_admin=message.from_user.username,
+                                   action=action,
+                                   time_action=datetime.now(),
+                                   )
+        admin_action.save()
+    else:
+        user_action = UserAction(id_user=message.from_user.id,
+                                 username=message.from_user.username,
+                                 action=action,
+                                 time_action=datetime.now(),
+                                 )
+        user_action.save()
