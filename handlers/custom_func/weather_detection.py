73af7a52d9b55df9d5e1bebba0ef5bca7b9d38cf
Комп с работы <vovka1998@gmail.com>
2024-03-25 15:40:19 +0300

    Скорректировал функцию. Добавил вариативность, когда город не указан

--- handlers/custom_func/weather_detection.py
+++ handlers/custom_func/weather_detection.py
@@ -2,21 +2,25 @@ import json
 from typing import Tuple
 
 
-
-def weather_detection(data_for_weather: json, city=None) -> tuple[str, str] | str:
+def weather_detection(data_for_weather: json, city=None) -> Tuple[str, str] | str:
     id_list_good_weather = [800, 801, 802, 600, 601]
     temp_in_city = data_for_weather['main']['temp']
     description_weather_in_city = data_for_weather['weather'][0]['description']
     if data_for_weather['weather'][0]['id'] in id_list_good_weather:
         if city:
             return ((f'Погода в городе {city} хорошая:\n'
-                    f'{description_weather_in_city} {temp_in_city}'),
+                     f'{description_weather_in_city} {temp_in_city}'),
                     f'Для сохранения хорошего настроения, получите фото с собачкой:)')
         else:
             return ((f'Погода в вашем городе хорошая:\n'
                      f'{description_weather_in_city} {temp_in_city}'),
                     f'Для сохранения хорошего настроения, получите фото с собачкой:)')
     else:
-        return ((f'Погода в городе {city} не очень:\n'
-                f'{description_weather_in_city} {temp_in_city}'),
-                f'Для поднятия настроения, получите фото с собачкой:)')
+        if city:
+            return ((f'Погода в городе {city} не очень:\n'
+                     f'{description_weather_in_city} {temp_in_city}'),
+                    f'Для поднятия настроения, получите фото с собачкой:)')
+        else:
+            return ((f'Погода в вашем городе не очень:\n'
+                     f'{description_weather_in_city} {temp_in_city}'),
+                    f'Для поднятия настроения, получите фото с собачкой:)')
