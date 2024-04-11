ed661275686bc42660ec184fdbe8fba103fbbe67
Комп с работы <vovka1998@gmail.com>
2024-04-11 16:51:59 +0300

    Убрал лишнее и скорректировал получение информации для составления логфайла

--- handlers/custom_func/log_func.py
+++ handlers/custom_func/log_func.py
@@ -28,8 +28,8 @@ def put_log_info(bot: TeleBot, message: Message, check_admin=False):
     if check_admin:
         pass
     else:
-        id_user = message.from_user.id
-        username = message.from_user.username
+        id_user = message.chat.id
+        username = message.chat.username
         user_report = message.text
         actions_user = ''
         for user in UserAction.select().where(UserAction.id_user == id_user):
@@ -47,47 +47,3 @@ def put_log_info(bot: TeleBot, message: Message, check_admin=False):
                                           f'id пользователя: {id_user}\n'
                                           f'Сообщение: {user_report}')
                 bot.send_document(id_chat, log_file)
-
-
-dicct = {'content_type': 'text', 'id': 1376, 'message_id': 1376,
-         'from_user': {'id': 437878719, 'is_bot': False, 'first_name': 'Владимир', 'username': 'ReBiwer',
-                       'last_name': 'Быков', 'language_code': 'ru', 'can_join_groups': None,
-                       'can_read_all_group_messages': None, 'supports_inline_queries': None, 'is_premium': None,
-                       'added_to_attachment_menu': None}, 'date': 1712754120,
-         'chat': {'id': 437878719, 'type': 'private', 'title': None, 'username': 'ReBiwer', 'first_name': 'Владимир',
-                  'last_name': 'Быков', 'is_forum': None, 'photo': None, 'bio': None, 'join_to_send_messages': None,
-                  'join_by_request': None, 'has_private_forwards': None,
-                  'has_restricted_voice_and_video_messages': None, 'description': None, 'invite_link': None,
-                  'pinned_message': None, 'permissions': None, 'slow_mode_delay': None,
-                  'message_auto_delete_time': None, 'has_protected_content': None, 'sticker_set_name': None,
-                  'can_set_sticker_set': None, 'linked_chat_id': None, 'location': None, 'active_usernames': None,
-                  'emoji_status_custom_emoji_id': None, 'has_hidden_members': None,
-                  'has_aggressive_anti_spam_enabled': None, 'emoji_status_expiration_date': None,
-                  'available_reactions': None, 'accent_color_id': None, 'background_custom_emoji_id': None,
-                  'profile_accent_color_id': None, 'profile_background_custom_emoji_id': None,
-                  'has_visible_history': None}, 'sender_chat': None, 'is_automatic_forward': None,
-         'reply_to_message': None, 'via_bot': None, 'edit_date': None, 'has_protected_content': None,
-         'media_group_id': None, 'author_signature': None, 'text': 'hj', 'entities': None, 'caption_entities': None,
-         'audio': None, 'document': None, 'photo': None, 'sticker': None, 'video': None, 'video_note': None,
-         'voice': None, 'caption': None, 'contact': None, 'location': None, 'venue': None, 'animation': None,
-         'dice': None, 'new_chat_members': None, 'left_chat_member': None, 'new_chat_title': None,
-         'new_chat_photo': None, 'delete_chat_photo': None, 'group_chat_created': None, 'supergroup_chat_created': None,
-         'channel_chat_created': None, 'migrate_to_chat_id': None, 'migrate_from_chat_id': None, 'pinned_message': None,
-         'invoice': None, 'successful_payment': None, 'connected_website': None, 'reply_markup': None,
-         'message_thread_id': None, 'is_topic_message': None, 'forum_topic_created': None, 'forum_topic_closed': None,
-         'forum_topic_reopened': None, 'has_media_spoiler': None, 'forum_topic_edited': None,
-         'general_forum_topic_hidden': None, 'general_forum_topic_unhidden': None, 'write_access_allowed': None,
-         'users_shared': None, 'chat_shared': None, 'story': None, 'external_reply': None, 'quote': None,
-         'link_preview_options': None, 'giveaway_created': None, 'giveaway': None, 'giveaway_winners': None,
-         'giveaway_completed': None, 'forward_origin': None, 'json': {'message_id': 1376,
-                                                                      'from': {'id': 437878719, 'is_bot': False,
-                                                                               'first_name': 'Владимир',
-                                                                               'last_name': 'Быков',
-                                                                               'username': 'ReBiwer',
-                                                                               'language_code': 'ru'},
-                                                                      'chat': {'id': 437878719,
-                                                                               'first_name': 'Владимир',
-                                                                               'last_name': 'Быков',
-                                                                               'username': 'ReBiwer',
-                                                                               'type': 'private'}, 'date': 1712754120,
-                                                                      'text': 'hj'}}
