c14dec2411e8e02f03d510b5429d286b6ce66c49
Комп с работы <vovka1998@gmail.com>
2024-04-11 14:12:26 +0300

    исправил опечатку

--- keyboards/admin_buttons/inline/user_choice.py
+++ keyboards/admin_buttons/inline/user_choice.py
@@ -4,6 +4,6 @@ from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 def user_choice() -> InlineKeyboardMarkup:
     keyboard = InlineKeyboardMarkup(row_width=2)
     but_user = InlineKeyboardButton('Как обычный пользователь', callback_data='user_direction')
-    but_admin = InlineKeyboardButton('Как обычный пользователь', callback_data='admin_direction')
+    but_admin = InlineKeyboardButton('Как администратор', callback_data='admin_direction')
     keyboard.add(but_user, but_admin)
     return keyboard
