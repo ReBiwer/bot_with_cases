76279ae85e51b4ddb4077a8b103038f0f06a3f4a
Комп с работы <vovka1998@gmail.com>
2024-04-11 13:03:46 +0300

    Исправил опечатку

--- keyboards/admin_buttons/inline/action_admin.py
+++ keyboards/admin_buttons/inline/action_admin.py
@@ -3,6 +3,6 @@ from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 def action_admin() -> InlineKeyboardMarkup:
     keyboard = InlineKeyboardMarkup(row_width=1)
-    but_1 = InlineKeyboardButton('Узнать погоду в в другом городе (через API)', callback_data='another_city')
+    but_1 = InlineKeyboardButton('Узнать погоду в другом городе (через API)', callback_data='another_city')
     keyboard.add(but_1)
     return keyboard
