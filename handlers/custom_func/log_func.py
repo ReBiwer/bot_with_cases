b91b0941677fc27f64cbcef36dfdce708fe435d0
Комп с работы <vovka1998@gmail.com>
2024-04-17 13:43:46 +0300

    Ограничил выгрузку логов. Теперь выгружаются логи за последний день. В дальнейшем можно будет добавить настройку свежести логов

--- handlers/custom_func/log_func.py
+++ handlers/custom_func/log_func.py
@@ -34,19 +34,24 @@ def put_log_info(bot: TeleBot, message: Message):
     if UserState.admin_access:
         pass
     else:
+        current_date = datetime.now()
+        format_date = '%Y-%m-%d %H:%M:%S'
+        day_ago: str = (f'{current_date.year}-{current_date.month}-{current_date.day-1} '
+                        f'{current_date.hour}:{current_date.minute}:{current_date.second}')
+        date_day_ago = datetime.strptime(day_ago, format_date)
         id_user = message.chat.id
         username = message.chat.username
         user_report = message.text
         actions_user = ''
-        for user in UserAction.select().where(UserAction.id_user == id_user):
+        for user in UserAction.select().where(UserAction.id_user == id_user and UserAction.time_action > date_day_ago):
             actions_user += (f'ID_user: {user.id_user} -- '
                              f'username: {user.username} -- '
                              f'action: {user.action} -- '
                              f'time_action: {user.time_action}\n')
         id_admins = [admin.id_admin for admin in Admins.select()]
         with open(f'handlers/custom_func/logs/{id_user}_logfile.log', 'w', encoding='utf-8') as log_file:
-            log_file.write(str(actions_user))
-        with open(f'handlers/custom_func/logs/{id_user}_logfile.log', 'r') as log_file:
+            log_file.write(actions_user)
+        with open(f'handlers/custom_func/logs/{id_user}_logfile.log', 'r', encoding='utf-8') as log_file:
             for id_chat in id_admins:
                 bot.send_message(id_chat, 'Пришел новый репорт')
                 bot.send_message(id_chat, f'Пользователь: {username}\n'
