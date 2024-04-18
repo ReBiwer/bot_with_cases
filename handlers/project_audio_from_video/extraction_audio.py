01334bb16bfe7cea340aee5557b285c9bb57248f
Комп с работы <vovka1998@gmail.com>
2024-04-18 10:45:56 +0300

    Скорректировал путь для сохранения загружаемого видео и отправляемого извлеченного аудио

--- handlers/project_audio_from_video/extraction_audio.py
+++ handlers/project_audio_from_video/extraction_audio.py
@@ -1,8 +1,9 @@
 from moviepy import editor
 from telebot.types import CallbackQuery, Message
-
 from handlers.custom_func.decorators import update_UserState_action
+from handlers.custom_func.log_func import log_action
 from loader import bot
+import os
 
 
 @bot.callback_query_handler(func=lambda call: call.data == 'extract_audio')
@@ -12,19 +13,32 @@ def start_extract_audio(call: CallbackQuery):
     Старт проекта 'аудио из видео'
     """
     message = call.message
+    log_action('Старт извлечения аудио. Запрос видео для обработки', message)
     response = bot.send_message(message.chat.id, 'Загрузите видео из которого вы хотите извлечь аудио')
     bot.register_next_step_handler(response, extraction_audio)
 
 
 @update_UserState_action
 def extraction_audio(message: Message):
+    log_action('Начало обработки видео.', message)
     bot.send_message(message.chat.id, 'Идет обработка файла, подождите немного...')
+    chat_id = message.chat.id
     path_video_from_mes = bot.get_file(message.video.file_id).file_path
     download_video = bot.download_file(path_video_from_mes)
-    with open(f'video_{message.chat.id}.mp4', 'wb') as file:
+    path_dir = os.path.abspath(os.path.join('handlers', 'project_audio_from_video', 'download_files', f'{chat_id}'))
+
+    if not os.path.isdir(path_dir):
+        os.mkdir(path_dir)
+
+    filename_video = f'{path_dir}\\{chat_id}_video.mp4'
+    filename_audio = f'{path_dir}\\{chat_id}_video.mp3'
+    with open(filename_video, 'wb') as file:
         file.write(download_video)
-    video_file = editor.VideoFileClip(f'video_{message.chat.id}.mp4')
-    video_file.audio.write_audiofile('audio_from_video.mp3')
-    bot.send_message(message.chat.id, 'Прошу, вот ваш файл')
-    with open('audio_from_video.mp3', 'rb') as audio:
-        bot.send_document(message.chat.id, audio)
+
+    video_file = editor.VideoFileClip(filename_video)
+    video_file.audio.write_audiofile(filename_audio)
+
+    bot.send_message(chat_id, 'Прошу, вот ваш файл')
+    with open(filename_audio, 'rb') as audio:
+        bot.send_document(chat_id, audio)
+    log_action('Аудио было извлечено успешно', message)
