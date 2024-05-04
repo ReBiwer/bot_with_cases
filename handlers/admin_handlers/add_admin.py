84f8c6ba52ca667ae9b38cb9cc71cc1ab4443cf3
ReBiwer <vovka1998@gmail.com>
2024-05-04 19:09:44 +0300

    Скорректировал функцию добавления нового админа

--- handlers/admin_handlers/add_admin.py
+++ handlers/admin_handlers/add_admin.py
@@ -9,15 +9,16 @@ from telebot.types import Message
 @update_UserState_action
 def add_admin(message: Message):
     id_all_admins = [admin.id_admin for admin in Admins.select()]
+    name_admin = message.chat.username if message.chat.username else message.chat.first_name
     if message.from_user.id not in id_all_admins:
         new_admin = Admins(id_admin=message.from_user.id,
-                           username_admin=message.from_user.username,)
+                           username_admin=name_admin,)
         bot.send_message(message.chat.id, f'Добавлен новый администратор:\n'
                                           f'id: {new_admin.id_admin}\n'
-                                          f'username: {new_admin.username_admin}')
+                                          f'name: {new_admin.username_admin}')
         new_admin.save()
         log_action(f'Команда - add_admin, new_admin - {new_admin.username_admin}', message)
     else:
-        bot.send_message(message.chat.id, f'Пользователь {message.from_user.username} '
+        bot.send_message(message.chat.id, f'Пользователь {name_admin} '
                                           f'уже является администратором этого бота')
-        log_action(f'Команда - add_admin, администратор {message.chat.username} уже есть', message)
+        log_action(f'Команда - add_admin, администратор {name_admin} уже есть', message)
