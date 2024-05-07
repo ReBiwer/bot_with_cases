669a44e4a54e8e04e05b87dcb4d0a73be33b91e2
rebiwer(kubuntu) <vovka1998@gmail.com>
2024-05-07 15:18:18 +0300

    Прописал логирование в функциях

--- handlers/project_file_encryption/encryption.py
+++ handlers/project_file_encryption/encryption.py
@@ -6,27 +6,43 @@ from loader import bot
 from states.user_state import UserState
 
 
-def request_file_for_encryption(message: Message):
+@bot.callback_query_handler(func=lambda call: call == "encrypt_file")
+@update_UserState_action
+def request_file_for_encryption(call: CallbackQuery):
     """Запрашиваем файл, который пользователь хочет зашифровать"""
-    pass
+    message = call.message
+    UserState.current_logger.info('Запрос файла для шифрования')
+    response = bot.send_message(message.chat.id, 'Загрузите файл формата txt, который вы хотите зашифровать')
+    bot.register_next_step_handler(response, get_file_for_encryption)
 
 
-def get_file_for_encryption(call: CallbackQuery):
-    """Получаем файл, который нужно зашифровать и запрашиваем пароль для шифрования.
+@update_UserState_action
+def get_file_for_encryption(message: Message):
+    """Получаем файл, который нужно зашифровать и запрашиваем пароль для шифрования"""
+    UserState.current_logger.info('Получение файла для шифрования. Запрос пароля')
+
+
+@update_UserState_action
+def send_encryption_file(message: Message):
+    """Получаем пароль для дешифровки файла и скидываем зашифрованный файл.
     P.S. написать пользователю, чтобы название файла не изменял"""
-    pass
+    UserState.current_logger.info('Отправка зашифрованного файла')
 
 
-def send_encryption_file(call: CallbackQuery):
-    """Получаем пароль для дешифровки файла и скидываем зашифрованный файл"""
-    pass
+@update_UserState_action
+def request_encryption_file(call: CallbackQuery):
+    """Получаем зашифрованный файл и запрашиваем пароль для дешифровки.
+    P.S. написать пользователю, что имя файла должно быть таким же, какое было присвоено этим ботом"""
+    UserState.current_logger.info('Запрос файла для дешифрования')
 
 
-def get_encryption_file(message: Message):
-    """Получаем зашифрованный файл и запрашиваем пароль для дешифровки"""
-    pass
+@update_UserState_action
+def get_password_for_decryption(message: Message):
+    """Получаем зашифрованный файл и запрашиваем пароль для дешифрования"""
+    UserState.current_logger.info('Получение файла для дешифрования. Запрос пароля')
 
 
-def send_decryption_file(call: CallbackQuery):
+@update_UserState_action
+def send_decryption_file(message: Message):
     """Получаем пароль для дешифровки файла, дешифруем файл и отправляем пользователю"""
-    pass
+    UserState.current_logger.info('Отправка дешифрованного файла')
