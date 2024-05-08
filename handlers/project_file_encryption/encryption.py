84ecb44e51a4bb02cd176edaee64dd7ce19e974a
rebiwer(kubuntu) <vovka1998@gmail.com>
2024-05-08 09:18:43 +0300

    Доработал функцию шифрования файла (любого файла)

--- handlers/project_file_encryption/encryption.py
+++ handlers/project_file_encryption/encryption.py
@@ -13,7 +13,7 @@ def request_file_for_encryption(call: CallbackQuery):
     """Запрашиваем файл, который пользователь хочет зашифровать"""
     message = call.message
     UserState.current_logger.info('Запрос файла для шифрования')
-    response = bot.send_message(message.chat.id, 'Загрузите файл формата txt, который вы хотите зашифровать')
+    response = bot.send_message(message.chat.id, 'Загрузите файл, который вы хотите зашифровать.')
     bot.register_next_step_handler(response, get_file_for_encryption)
 
 
@@ -22,7 +22,12 @@ def get_file_for_encryption(message: Message):
     """Получаем файл, который нужно зашифровать и запрашиваем пароль для шифрования"""
     UserState.current_logger.info('Получение файла для шифрования. Запрос пароля')
     chat_id = message.chat.id
-    path_file = bot.get_file(message.document.file_id).file_path
+    if message.document:
+        path_file = bot.get_file(message.document.file_id).file_path
+    elif message.video:
+        path_file = bot.get_file(message.video.file_id).file_path
+    else:
+        path_file = bot.get_file(message.photo[-1].file_id).file_path
     UserState.path_to_file_for_encryption = path_file
     response = bot.send_message(chat_id, 'Прошу введите пароль. '
                                          'Он понадобиться, когда вы захотите дешифровать свой файл'
@@ -38,18 +43,26 @@ def send_encryption_file(message: Message):
     """
     UserState.current_logger.info('Пароль был получен. Начало обработки файла')
 
-    password = message.text
-    chat_id = message.chat.id
+    #  Получаем пароль, ID чата и сохраняем пароль в атрибуте UserState для сохранения
+    password: str = message.text
+    chat_id: int = message.chat.id
     UserState.password_to_encryption_file = password
+    #  Получаем путь к файлу и его расширение полученному ботом от пользователя
+    path_file, file_extension = os.path.splitext(UserState.path_to_file_for_encryption)
 
-    path_file = UserState.path_to_file_for_encryption
+    #  Скачиваем файл
+    download_file_for_encryption: bytes = bot.download_file(path_file + file_extension)
 
-    download_file_for_encryption = bot.download_file(path_file)
-    path_dir = os.path.abspath(
-        os.path.join('handlers', 'project_file_encryption', 'download_files', f'{chat_id}_user')
+    #  Разделям путь к файлу на директорю "download" и название файла
+    dir_to_file, filename = os.path.split(path_file)
+
+    #  Создаем путь к директории где будет храниться скаченный файл
+    path_dir: str = os.path.abspath(
+        os.path.join('handlers', 'project_file_encryption', 'download_files', f'{dir_to_file}_{UserState.username}')
     )
-    path_to_file = os.path.join(path_dir, f'{chat_id}_file.txt')
-    path_to_encrypt_file = os.path.join(path_dir, f'{chat_id}_file.aes')
+    path_to_file: str = os.path.join(path_dir, f'{chat_id}_{filename}{file_extension}')
+    path_to_encrypt_file = os.path.join(path_dir, f'{chat_id}_{filename}.aes')
+    UserState.path_to_file_for_encryption = path_to_encrypt_file
 
     if not os.path.isdir(path_dir):
         UserState.current_logger.info('Создание директории с файлами пользователя')
